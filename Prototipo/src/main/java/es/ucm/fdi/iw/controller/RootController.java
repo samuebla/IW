12719c7c235d668a5f8edec8a3a4656d85919004
mihern07 <mihern07@ucm.es>
2023-02-18 10:36:56 +0100

    Añadidas paginas de Como jugar y records a la navegacion y editadas para comp con Thymeleaf

--- Prototipo/src/main/java/es/ucm/fdi/iw/controller/RootController.java
+++ Prototipo/src/main/java/es/ucm/fdi/iw/controller/RootController.java
@@ -7,19 +7,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
- *  Non-authenticated requests only.
+ * Non-authenticated requests only.
  */
 @Controller
 public class RootController {
 
-	private static final Logger log = LogManager.getLogger(RootController.class);
+    private static final Logger log = LogManager.getLogger(RootController.class);
 
-	@GetMapping("/login")
+    @GetMapping("/login")
     public String login(Model model) {
         return "login";
     }
 
-	@GetMapping("/")
+    @GetMapping("/comojugar")
+    public String comojugar(Model model) {
+        return "comojugar";
+    }
+
+    @GetMapping("/records")
+    public String records(Model model) {
+        return "records";
+    }
+
+    @GetMapping("/")
     public String index(Model model) {
         return "index";
     }
