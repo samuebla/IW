7d0f0cd7fe7adee19a84c8d5bfaf35dc08589b7c
Sebana02 <sebana@ucm.es>
2023-02-21 14:30:57 +0100

    confusion listausuarios

--- Prototipo/src/main/java/es/ucm/fdi/iw/controller/RootController.java
+++ Prototipo/src/main/java/es/ucm/fdi/iw/controller/RootController.java
@@ -14,12 +14,6 @@ public class RootController {
 
     private static final Logger log = LogManager.getLogger(RootController.class);
 
-
-    @GetMapping("/listaUsuarios")
-    public String listaUsuarios(Model model){
-        return "listaUsuarios";
-    }
-    
     @GetMapping("/partida")
     public String partida(Model model){
         return "partida";
