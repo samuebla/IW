5d22cfb3ccd59c4127decab4b347ebab0fdf0a0a
Sebana02 <sebana@ucm.es>
2023-03-21 14:37:27 +0100

    canvas reescalado
    Co-Authored-By: adriac02 <62616973+adriac02@users.noreply.github.com>

--- Prototipo/src/main/resources/static/js/game/game.js
+++ Prototipo/src/main/resources/static/js/game/game.js
@@ -1,16 +1,40 @@
 //Configuracioón del juego de Phaser
+
 let config = {
     type: Phaser.CANVAS,
     canvas: document.getElementById("juego"),
-    width: 1280,
-    height: 720,
+    width: 1000,
+    height: 1000,
     backgroundColor: "#dadaad",
     scale: {
         mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
     },
     pixelArt: true,
-    scene: []
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
 };
 
 new Phaser.Game(config);
+
+function preload() {
+    this.load.setPath('/assets/');
+    this.load.spritesheet("Black", "BlackPieces.png", {
+        frameWidth: 16,
+        frameHeight: 16
+    });
+    this.load.image("Board", "board_plain_01.png");
+}
+
+function create() {
+    this.add.sprite(100, 100, "Board");
+
+
+    this.add.sprite(100, 100, "Black", 0)
+        .setScale(2);
+
+}
+
+function update() { }
