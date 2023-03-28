0b839f8d6c810b369ae708de49ea71967367b67f
mihern07 <mihern07@ucm.es>
2023-03-28 13:16:25 +0200

    Cambios de Adrià de cuando no funcionaba GitHub sin los assets
    Co-Authored-By: adriac02 <62616973+adriac02@users.noreply.github.com>

---
+++ Prototipo/src/main/resources/static/js/scenes/boot.js
@@ -0,0 +1,37 @@
+export default class Boot extends Phaser.Scene {
+
+    constructor() {
+        super({ key: "boot" });
+    }
+
+    //carga de assets
+    preload() {
+        this.load.setPath('/assets/');
+
+        this.load.spritesheet("black_pieces", "black_pieces.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        });
+
+        this.load.spritesheet("white_pieces", "white_pieces.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        });
+
+        this.load.spritesheet("blue_pieces", "blue_pieces.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        });
+
+        this.load.spritesheet("red_pieces", "red_pieces.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        });
+
+        this.load.image("board", "board.png");
+    }
+
+    create() {
+        this.scene.start("fourdrez");
+    }
+}
