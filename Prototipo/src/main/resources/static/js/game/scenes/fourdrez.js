1e586b14abce5006eaffb90a596f63d1ccd6b4f1
adriac02 <adriac02@ucm.es>
2023-04-11 13:45:49 +0200

    Movimiento por turnos

--- Prototipo/src/main/resources/static/js/game/scenes/fourdrez.js
+++ Prototipo/src/main/resources/static/js/game/scenes/fourdrez.js
@@ -7,6 +7,7 @@ export default class Fourdrez extends Phaser.Scene {
     preload() {
         this.players = [];
         this.board = [];
+        this.turn = 0;
     }
 
     create() {
@@ -31,13 +32,13 @@ export default class Fourdrez extends Phaser.Scene {
             for (let j = 0; j < nPieces; j++) {
                 let sprite;
 
-                if (i === 0)
+                if (i === 2)
                     sprite = this.add.sprite(offset + ((j % 8) * 18), 8 + (20 * (j < 8 ? 1 : 0)), "black_pieces", spriteMap[j]);
 
-                else if (i === 1)
+                else if (i === 0)
                     sprite = this.add.sprite(offset + ((j % 8) * 18), (256 - 28) + (20 * (j < 8 ? 0 : 1)), "white_pieces", spriteMap[j]);
 
-                else if (i === 2)
+                else if (i === 1)
                     sprite = this.add.sprite(8 + (20 * (j < 8 ? 1 : 0)), offset + ((j % 8) * 18), "red_pieces", spriteMap[j]);
 
                 else if (i === 3)
@@ -46,17 +47,20 @@ export default class Fourdrez extends Phaser.Scene {
 
                 sprite.setInteractive();
 
-                //sprite.setScale()
                 sprite.on('pointerdown', (pointer) => {
-                    if(!this.players[0].movingPiece){
-                        this.players[0].movingPiece = true;
-                        this.players[0].pieceToMove = sprite;
+                    if(!this.players[this.turn].movingPiece && this.turn === i){
+                        this.players[this.turn].movingPiece = true;
+                        this.players[this.turn].pieceToMove = sprite;
                     }
+                    
                 });
 
                 sprite.on('pointerover', (pointer) => {
-                    sprite.setScale(1.5);
-                    sprite.setDepth(1);
+                    if(this.turn === i){
+                        sprite.setScale(1.5);
+                        sprite.setDepth(1);
+                    }
+                    
                 });
 
                 sprite.on('pointerout', (pointer) => {
@@ -129,13 +133,24 @@ class Casilla extends Phaser.GameObjects.Rectangle {
         super(scene, x, y, width, height, fillColor);
         scene.add.existing(this);
         this.setOrigin(0, 0);
+        this.pieza = null;
 
         this.setInteractive();
         this.on('pointerdown', (pointer) => {
-            if(scene.players[0].movingPiece){
-                scene.players[0].pieceToMove.x = x + 8;
-                scene.players[0].pieceToMove.y = y + 8;
-                scene.players[0].movingPiece = false;
+            if(scene.players[scene.turn].movingPiece){
+                scene.players[scene.turn].pieceToMove.x = x + 8;
+                scene.players[scene.turn].pieceToMove.y = y + 8;
+                // if(this.pieza !== null){
+                //     Destroy(this.pieza);
+                //     this.pieza = scene.players[scene.turn].pieceToMove;
+                // }
+                // else{
+                //     this.pieza = scene.players[scene.turn].pieceToMove;
+                // }
+                
+                scene.players[scene.turn].movingPiece = false;
+                if(scene.turn + 1 > 3) scene.turn = 0;
+                else scene.turn++;
             }
         });
 
