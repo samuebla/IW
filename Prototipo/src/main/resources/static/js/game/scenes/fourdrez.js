95de79194d20ed59f606cd0a51bf265c581576bb
adriac02 <adriac02@ucm.es>
2023-04-17 14:38:35 +0200

    Avanzados movimientos de piezas

--- Prototipo/src/main/resources/static/js/game/scenes/fourdrez.js
+++ Prototipo/src/main/resources/static/js/game/scenes/fourdrez.js
@@ -6,7 +6,7 @@ export default class Fourdrez extends Phaser.Scene {
 
     preload() {
         this.players = [];
-        this.board = [];
+        this.board = Array(14).fill(Array(14));
         this.turn = 0;
     }
 
@@ -32,17 +32,30 @@ export default class Fourdrez extends Phaser.Scene {
             for (let j = 0; j < nPieces; j++) {
                 let sprite;
 
-                if (i === 2)
-                    this.players[i].piezas[j] = new Pieza(this, this.add.sprite(offset + ((j % 8) * 18), 8 + (20 * (j < 8 ? 1 : 0)), "black_pieces", spriteMap[j]), 0);
-
-                else if (i === 0)
-                    this.players[i].piezas[j] = new Pieza(this, this.add.sprite(offset + ((j % 8) * 18), (256 - 28) + (20 * (j < 8 ? 0 : 1)), "white_pieces", spriteMap[j]), 0);
-
-                else if (i === 1)
-                    this.players[i].piezas[j] = new Pieza(this, this.add.sprite(8 + (20 * (j < 8 ? 1 : 0)), offset + ((j % 8) * 18), "red_pieces", spriteMap[j]), 0);
-
-                else if (i === 3)
-                    this.players[i].piezas[j] = new Pieza(this, this.add.sprite((256 - 28) + (20 * (j < 8 ? 0 : 1)), offset + ((j % 8) * 18), "blue_pieces", spriteMap[j]), 0);
+                if (i === 2){
+                    let x = (j % 8) + 3;
+                    let y = j < 8 ? 1 : 0;
+                    this.players[i].piezas[j] = new Pieza(this, this.add.sprite(offset + ((j % 8) * 18), 8 + (20 * (j < 8 ? 1 : 0)), "black_pieces", spriteMap[j]), j, i);
+                    this.board[x][y].setPieza(this.players[i].piezas[j],x,y);
+                }
+                else if (i === 0){
+                    let x = (j % 8) + 3;
+                    let y = j < 8 ? 12 : 13;
+                    this.players[i].piezas[j] = new Pieza(this, this.add.sprite(offset + ((j % 8) * 18), (256 - 28) + (20 * (j < 8 ? 0 : 1)), "white_pieces", spriteMap[j]), j, i);
+                    this.board[x][y].setPieza(this.players[i].piezas[j],x,y);
+                }
+                else if (i === 1){
+                    let x = (j < 8 ? 1 : 0);
+                    let y = (j % 8) + 3;
+                    this.players[i].piezas[j] = new Pieza(this, this.add.sprite(8 + (20 * (j < 8 ? 1 : 0)), offset + ((j % 8) * 18), "red_pieces", spriteMap[j]), j, i);
+                    this.board[x][y].setPieza(this.players[i].piezas[j],x,y);
+                }
+                else if (i === 3){
+                    let x = (j < 8 ? 12 : 13);
+                    let y = (j % 8) + 3;
+                    this.players[i].piezas[j] = new Pieza(this, this.add.sprite((256 - 28) + (20 * (j < 8 ? 0 : 1)), offset + ((j % 8) * 18), "blue_pieces", spriteMap[j]), j, i);
+                    this.board[x][y].setPieza(this.players[i].piezas[j],x,y);
+                }
             }
         }
 
@@ -80,13 +93,13 @@ class Tablero {
             this.changeColor(white, black);
             for (let i = 3; i < this.filas - 3; i++) {
                 this.changeColor(white, black);
-                new Casilla(this.scene, i * this.sizeFila + this.x, j * this.sizeColumna + this.y, this.sizeFila, this.sizeColumna, this.color);
+                this.scene.board[i][j] = new Casilla(this.scene, i * this.sizeFila + this.x, j * this.sizeColumna + this.y, this.sizeFila, this.sizeColumna, this.color);
             }
         }
 
         for (let j = 3; j < this.columnas - 3; j++) {
             for (let i = 0; i < 3; i++) {
-                new Casilla(this.scene, i * this.sizeFila + this.x, j * this.sizeColumna + this.y, this.sizeFila, this.sizeColumna, this.color);
+                this.scene.board[i][j] = new Casilla(this.scene, i * this.sizeFila + this.x, j * this.sizeColumna + this.y, this.sizeFila, this.sizeColumna, this.color);
                 this.changeColor(white, black);
             }
         }
@@ -95,7 +108,7 @@ class Tablero {
 
         for (let j = 3; j < this.columnas - 3; j++) {
             for (let i = this.filas - 3; i < this.filas; i++) {
-                new Casilla(this.scene, i * this.sizeFila + this.x, j * this.sizeColumna + this.y, this.sizeFila, this.sizeColumna, this.color);
+                this.scene.board[i][j] = new Casilla(this.scene, i * this.sizeFila + this.x, j * this.sizeColumna + this.y, this.sizeFila, this.sizeColumna, this.color);
                 this.changeColor(white, black);
             }
         }
@@ -113,28 +126,34 @@ class Casilla extends Phaser.GameObjects.Rectangle {
         scene.add.existing(this);
         this.setOrigin(0, 0);
         this.pieza = null;
+        this.originalColor = fillColor;
+
+        this.possible = false;
 
         this.setInteractive();
         this.on('pointerdown', (pointer) => {
-            if(scene.players[scene.turn].movingPiece){
-                scene.players[scene.turn].disablePieces();
-                if(this.pieza !== null){
-                    this.pieza.destroy();
-                    this.pieza = scene.players[scene.turn].pieceToMove;
+            if(this.possible){
+                if(scene.players[scene.turn].movingPiece){
+                    scene.players[scene.turn].disablePieces();
+                    if(this.pieza.sprite !== null){
+                        this.pieza.sprite.destroy();
+                        this.pieza = scene.players[scene.turn].pieceToMove;
+                    }
+                    else{
+                        this.pieza = scene.players[scene.turn].pieceToMove;
+                    }
+    
+                    scene.players[scene.turn].pieceToMove.sprite.x = x + 8;
+                    scene.players[scene.turn].pieceToMove.sprite.y = y + 8;
+                    scene.players[scene.turn].movingPiece = false;
+    
+                    if(scene.turn + 1 > 3) scene.turn = 0;
+                    else scene.turn++;
+    
+                    scene.players[scene.turn].interactPieces();
                 }
-                else{
-                    this.pieza = scene.players[scene.turn].pieceToMove;
-                }
-
-                scene.players[scene.turn].pieceToMove.x = x + 8;
-                scene.players[scene.turn].pieceToMove.y = y + 8;
-                scene.players[scene.turn].movingPiece = false;
-
-                if(scene.turn + 1 > 3) scene.turn = 0;
-                else scene.turn++;
-
-                scene.players[scene.turn].interactPieces();
             }
+            
         });
 
         this.on('pointerover', (pointer) => {
@@ -144,8 +163,23 @@ class Casilla extends Phaser.GameObjects.Rectangle {
         });
 
         this.on('pointerout', (pointer) => {
-            this.fillColor = fillColor;
+            this.fillColor = this.originalColor;
         });
+    }
+
+    colorPossible(){
+        this.possible = true;
+        this.fillColor = 0x6a6a6a;
+    }
+
+    colorReset(){
+        this.fillColor = this.originalColor;
+    }
+
+    setPieza(p, x, y){
+        this.pieza = p;
+        this.pieza.tableroX = x;
+        this.pieza.tableroY = y;
     }
 }
 
@@ -173,16 +207,33 @@ class Player{
 }
 
 class Pieza{
-    constructor(scene, s, t){
+    constructor(scene, s, t, e){
         this.scene = scene;
         this.tipo = t;
+        this.equipo;
+
+        this.tableroX;
+        this.tableroY;
+
         this.sprite = s;
         this.sprite.setInteractive();
 
         this.sprite.on('pointerdown', (pointer) => {
             if(!scene.players[scene.turn].movingPiece){
                 scene.players[scene.turn].movingPiece = true;
-                scene.players[scene.turn].pieceToMove = this.sprite;
+                scene.players[scene.turn].pieceToMove = this;
+                scene.board[this.tableroX][this.tableroY].colorPossible();
+
+                if(scene.turn === 0){
+                    if(this.tipo < 8){
+                        if(this.tableroY === 12){
+                            console.log(scene.board);
+                            console.log(this.tableroX, this.tableroY);
+                           
+                            scene.board[this.tableroX][this.tableroY].colorPossible();
+                        }
+                    }
+                }
             }
             
         }).disableInteractive();
