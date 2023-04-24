2619d062009c76b52816134beb5eb4e901110bd7
samuebla <samuebla@ucm.es>
2023-04-24 14:01:32 +0200

    Puedes Expulsar usuarios

--- Prototipo/src/main/java/es/ucm/fdi/iw/controller/AdminController.java
+++ Prototipo/src/main/java/es/ucm/fdi/iw/controller/AdminController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import javax.servlet.http.HttpSession;
+import javax.transaction.Transactional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,9 +40,41 @@ public class AdminController {
     }
 
     @GetMapping("/listausuarios")
-    public String listausuarios(Model model) {
+    public String listausuarios(Model model, HttpSession session) {
 
         model.addAttribute("usuarios", entityManager.createQuery("SELECT u FROM User u").getResultList());
+
+        //Cogemos el usuario
+        User u = entityManager.find(User.class, ((User) session.getAttribute("u")).getId());
+
+        //Y lo añadimos al modelo
+        model.addAttribute("currentUser",u);
+
+        return "listausuarios";
+    }
+
+    //Expulsamos a un usuario de la base de datos
+    @Transactional
+    @PostMapping("/listausuarios/echar")
+    public String listausuarios(Model model, HttpSession session,@RequestParam long id_reported) {
+
+        //Mantenemos los atributo de ListaUsuarios...
+
+        //Cogemos el usuario
+        User u = entityManager.find(User.class, ((User) session.getAttribute("u")).getId());
+        
+        //Y lo añadimos al modelo
+        model.addAttribute("currentUser",u);
+
+        //Cogemos el usuario que vayamos a expulsar
+        User user_reported = entityManager.find(User.class, id_reported);
+
+        //Y lo eliminamos
+        entityManager.remove(user_reported);
+
+        //Una vez eliminado, buscamos a los usuarios que quedan para mostrarlos
+        model.addAttribute("usuarios", entityManager.createQuery("SELECT u FROM User u").getResultList());
+
         return "listausuarios";
     }
 
